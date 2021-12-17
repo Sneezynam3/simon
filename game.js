c90782bf017de22e67cdcc5578042fbe9f7e2a37
James Snell <Vulpes3.1415@yahoo.com>
2021-12-16 18:34:14 -0700

    copied css from youtube but it is now close to functional

--- game.js
+++ game.js
@@ -19,6 +19,44 @@ const strictButton = document.querySelector("#strict");
 const onButton = document.querySelector("#on");
 const startButton = document.querySelector("#start");
 
-strictButton.addEventListener('change', (event) => {
+strictButton.addEventListener('click', (event) => {
     console.log("checked")
-})
+    if (strictButton.checked == true) {
+        strict == true;
+    } else {
+        strict == false;
+    }
+});
+
+onButton.addEventListener('click', (event) => {
+    if (onButton.checked == true) {
+        on = true;
+        turnCounter.innerHTML = "--";
+    } else {
+        on = false;
+        turnCounter.innerHTML = "";
+        clearColor();
+        clearInterval(intervalId)
+    }
+});
+
+strictButton.addEventListener('click', (event) => {
+    if(on || win) {
+        play();
+    }
+});
+
+function play () {
+  win = false;
+  order = [];
+  playerOrder = [];
+  flash = 0;
+  intervalId = 0;
+  turn = 1;
+  turnCounter.innerHTML = 1;
+  good = true;
+  for (var i=0; i < 20; i++) {
+      order.push(Math.floor(Math.random() *4) + 1)
+  }
+  console.log(order)
+}
